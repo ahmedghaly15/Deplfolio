98333a3fcc3484ef0e9b38103390f75e1dd01ac9
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-05-30 18:24:26 +0300

    Finished login UI logic ✅

---
+++ lib/features/login/presentation/providers/login_provider.dart
@@ -0,0 +1,42 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../../../core/supabase/supabase_request_result.dart';
+import '../../data/models/login_request_params.dart';
+import '../../data/repository/login_repo.dart';
+import 'form_providers.dart';
+
+part 'login_provider.g.dart';
+
+@riverpod
+class Login extends _$Login {
+  @override
+  AsyncValue<String>? build() => null;
+
+  void _login() async {
+    state = const AsyncValue.loading();
+    final result = await ref
+        .read(loginRepoProvider)
+        .loginViaEmailAndPass(
+          ref,
+          LoginRequestParams(
+            email: ref.read(emailControllerProvider).text,
+            password: ref.read(passControllerProvider).text.trim(),
+          ),
+        );
+    switch (result) {
+      case SupabaseRequestSuccess(data: final userId):
+        state = AsyncValue.data(userId);
+      case SupabaseRequestFailure(:final errorModel):
+        state = AsyncValue.error(errorModel.message, StackTrace.current);
+    }
+  }
+
+  void validateFormAndLogin() {
+    final formKey = ref.read(loginFormKeyProvider);
+    if (formKey.currentState!.validate()) {
+      _login();
+    } else {
+      ref.read(autovalidateModeProvider.notifier).enable();
+    }
+  }
+}
---
+++ lib/features/login/presentation/providers/login_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$loginHash() => r'b217776372e0b98a27ab0fefd5722be0578833de';
+
+/// See also [Login].
+@ProviderFor(Login)
+final loginProvider =
+    AutoDisposeNotifierProvider<Login, AsyncValue<String>?>.internal(
+      Login.new,
+      name: r'loginProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product') ? null : _$loginHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$Login = AutoDisposeNotifier<AsyncValue<String>?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
