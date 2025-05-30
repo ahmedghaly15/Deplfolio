b3e8d1e64ea7e0b443e9424cb51cd90d7de04f1f
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-05-30 13:44:59 +0300

    Added AppRegex, InputValidator helpers

---
+++ lib/core/helpers/input_validator.dart
@@ -0,0 +1,70 @@
+import 'app_regex.dart';
+
+class InputValidator {
+  InputValidator._();
+
+  static String? validatingEmailField({String? value}) {
+    if (value == null || value.isEmpty) {
+      return "Email can't be blank!";
+    } else if (!AppRegex.isEmailValid(value)) {
+      return "Please enter a valid email";
+    }
+    return null;
+  }
+
+  static String? validatingPasswordField({String? value}) {
+    if (value == null || value.isEmpty) {
+      return "Password can't be blank!";
+    } else if (!AppRegex.isPasswordValid(value)) {
+      return "Please enter a valid password";
+    }
+    return null;
+  }
+
+  static String? validateOptionalPassword({String? value}) {
+    if (value == null || value.isEmpty) {
+      // User did not enter a password; considered valid (no update intended)
+      return null;
+    } else if (!AppRegex.isPasswordValid(value)) {
+      return "Please enter a valid password";
+    }
+    return null;
+  }
+
+  static String? validateEgPhoneField({String? value}) {
+    if (value == null || value.isEmpty) {
+      return "Phone can't be blank!";
+    } else if (!AppRegex.isEgPhoneNumberValid(value)) {
+      return 'Please enter a valid phone number';
+    }
+    return null;
+  }
+
+  static String? validatingNameField(String? value) {
+    if (value == null || value.isEmpty) {
+      return "Name can't be blank!";
+    } else if (value.length < 2) {
+      return "Name must be at least 2 characters long";
+    }
+    return null;
+  }
+
+  static String? validatingConfirmPasswordField({
+    String? password,
+    String? confirmPassword,
+  }) {
+    if (confirmPassword == null || confirmPassword.isEmpty) {
+      return "Confirm password can't be blank!";
+    } else if (password != confirmPassword) {
+      return "Passwords do not match";
+    }
+    return null;
+  }
+
+  static String? validatingEmptyField(String? value) {
+    if (value == null || value.isEmpty) {
+      return "This field can't be blank!";
+    }
+    return null;
+  }
+}
