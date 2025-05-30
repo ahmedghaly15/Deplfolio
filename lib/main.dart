2250b9f0e7a00e1bf9f77b9567cbd619141a8bdf
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-05-31 01:47:57 +0300

    Updated main & Added AppNavigator extension

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart' show ProviderScope;
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'core/utils/functions/check_if_user_is_logged_in.dart';
 import 'deplfolio_app.dart';
 
 void main() async {
@@ -17,5 +18,6 @@ void main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+  await checkIfUserIsLoggedIn();
   runApp(const ProviderScope(child: DeplfolioApp()));
 }
