761efac847507d4ff07fa79f6386f9c966583a56
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-05-30 16:00:47 +0300

    Finished Login UI ✅

--- lib/features/login/presentation/view/login_view.dart
+++ lib/features/login/presentation/view/login_view.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/theming/text_styles_manager.dart';
+import '../../../../core/utils/app_strings.dart';
+import '../../../../core/widgets/primary_button.dart';
+import 'widgets/login_form_consumer.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Login View')));
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 24.h),
+        child: CustomScrollView(
+          physics: const BouncingScrollPhysics(),
+          slivers: [
+            SliverToBoxAdapter(
+              child: FittedBox(
+                child: Text(
+                  AppStrings.welcomeBack,
+                  style: TextStylesManager.font24SemiBold,
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: FittedBox(
+                child: Text(
+                  AppStrings.welcomeBackWeMissedYou,
+                  style: TextStylesManager.font12Regular,
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 16.h),
+                child: const LoginFormConsumer(),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: PrimaryButton(text: AppStrings.signIn, onPressed: () {}),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
