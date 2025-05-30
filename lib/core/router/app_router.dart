accae15ca30f5ecfcdec748b5ab7cf6ee49a20e8
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-05-30 12:56:44 +0300

    Updated AppStrings & AppRouter

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,19 +1,24 @@
 import 'package:deplfolio/features/login/presentation/view/login_view.dart';
 import 'package:flutter/material.dart';
 
-import '../theming/app_text_styles.dart';
+import '../theming/text_styles_manager.dart';
 import '../utils/app_strings.dart';
 import 'routes.dart';
 
 class AppRouter {
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
+      case '/':
+        return _loginRoute();
       case Routes.login:
-        return MaterialPageRoute(builder: (_) => LoginView());
+        return _loginRoute();
       default:
         return _unFoundRoute();
     }
   }
+
+  static MaterialPageRoute<dynamic> _loginRoute() =>
+      MaterialPageRoute(builder: (_) => const LoginView());
 
   static Route<dynamic> _unFoundRoute() {
     return MaterialPageRoute(
@@ -22,7 +27,7 @@ class AppRouter {
             body: Center(
               child: Text(
                 AppStrings.unFoundRoute,
-                style: AppTextStyles.font24Medium,
+                style: TextStylesManager.font18Medium,
               ),
             ),
           ),
--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -1,6 +1,10 @@
 class AppStrings {
   AppStrings._();
 
-  static const String appTitle = 'Deplfolio';
   static const String unFoundRoute = 'Un found route';
+  static const String welcomeBack = 'Welcome Back!';
+  static const String welcomeBackWeMissedYou = 'Welcome back, we missed you!';
+  static const String username = 'Username';
+  static const String password = 'Password';
+  static const String signIn = 'Sign In';
 }
