0b4ea91e5a81578af1ea9368cddfab0cbfd8b21d
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-05-30 12:58:39 +0300

    Updated main method & DeplfolioApp

--- lib/deplfolio_app.dart
+++ lib/deplfolio_app.dart
@@ -1,17 +1,31 @@
+import 'package:deplfolio/core/utils/const_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart' show ScreenUtilInit;
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 import 'core/router/app_router.dart';
-import 'core/utils/app_strings.dart';
 
 class DeplfolioApp extends StatelessWidget {
   const DeplfolioApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: AppStrings.appTitle,
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: AppRouter.onGenerateRoute,
+    return ShadApp.custom(
+      themeMode: ThemeMode.dark,
+      appBuilder:
+          (context) => ScreenUtilInit(
+            designSize: const Size(375, 812),
+            minTextAdapt: true,
+            splitScreenMode: true,
+            builder:
+                (context, _) => MaterialApp(
+                  theme: Theme.of(context),
+                  title: ConstStrings.appTitle,
+                  debugShowCheckedModeBanner: false,
+                  onGenerateRoute: AppRouter.onGenerateRoute,
+                  builder: (context, child) => ShadAppBuilder(child: child),
+                ),
+          ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart' show ProviderScope;
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'deplfolio_app.dart';
 
-void main() => runApp(const DeplfolioApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // To fix texts being hidden bug in release mode
+  await ScreenUtil.ensureScreenSize();
+  SystemChrome.setEnabledSystemUIMode(
+    SystemUiMode.manual,
+    overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top],
+  );
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(const ProviderScope(child: DeplfolioApp()));
+}
