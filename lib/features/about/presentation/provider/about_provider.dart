2a8184c35788f204120ad031501b585848f03834
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-06-01 02:18:46 +0300

    Refactored fetchAboutProvider logic

--- lib/features/about/presentation/provider/about_provider.dart
+++ lib/features/about/presentation/provider/about_provider.dart
@@ -1,11 +1,15 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../../../core/supabase/supabase_request_result.dart';
+import '../../data/models/about.dart';
 import '../../data/repository/about_repo.dart';
 
-final fetchAboutProvider = FutureProvider.autoDispose<SupabaseRequestResult>((
-  ref,
-) {
-  final aboutRepo = ref.watch(aboutRepoProvider);
-  return aboutRepo.fetchAbout(ref);
+final fetchAboutProvider = FutureProvider.autoDispose<About?>((ref) async {
+  final result = await ref.read(aboutRepoProvider).fetchAbout(ref);
+  switch (result) {
+    case SupabaseRequestSuccess(:final data):
+      return data;
+    case SupabaseRequestFailure(:final errorModel):
+      throw errorModel.message;
+  }
 });
