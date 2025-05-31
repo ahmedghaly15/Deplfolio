9343012e6f127803f70c6656a1ed2b8061f7036b
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-06-01 02:22:32 +0300

    Built IntroductionSection model

---
+++ lib/features/about/data/models/introduction_section.dart
@@ -0,0 +1,13 @@
+import 'about.dart';
+
+class IntroductionSection {
+  final String headerSmallText, description, seeMyWorkLink;
+  final AboutHeaderTextModel headerBigText;
+
+  IntroductionSection({
+    required this.headerSmallText,
+    required this.description,
+    required this.seeMyWorkLink,
+    required this.headerBigText,
+  });
+}
