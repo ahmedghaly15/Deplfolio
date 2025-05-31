05a3650e59fb5af176dc2643f669ab5fa97990b9
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-05-31 19:28:36 +0300

    Built layoutActiveWidgetProvider & Updated LayoutViewBodyConsumer widget

--- lib/features/layout/presentation/provider/layout_provider.dart
+++ lib/features/layout/presentation/provider/layout_provider.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../about/presentation/view/about_view.dart';
+import '../../../portfolio/presentation/view/portfolio_view.dart';
+import '../../../skills/presentation/view/skills_view.dart';
 
 class LayoutActiveTabNotifier extends StateNotifier<int> {
   LayoutActiveTabNotifier() : super(0);
@@ -10,3 +15,9 @@ final layoutActiveTabProvider =
     StateNotifierProvider.autoDispose<LayoutActiveTabNotifier, int>(
       (_) => LayoutActiveTabNotifier(),
     );
+
+final layoutActiveWidgetProvider = Provider.autoDispose<Widget>((ref) {
+  const List<Widget> views = [AboutView(), SkillsView(), PortfolioView()];
+  final activeTabIndex = ref.watch(layoutActiveTabProvider);
+  return views[activeTabIndex];
+});
--- lib/features/layout/presentation/view/widgets/layout_view_body_consumer.dart
+++ lib/features/layout/presentation/view/widgets/layout_view_body_consumer.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../../../about/presentation/view/about_view.dart';
-import '../../../../portfolio/presentation/view/portfolio_view.dart';
-import '../../../../skills/presentation/view/skills_view.dart';
 import '../../provider/layout_provider.dart';
 
 class LayoutViewBodyConsumer extends ConsumerWidget {
@@ -11,13 +8,6 @@ class LayoutViewBodyConsumer extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final activeTabIndex = ref.watch(layoutActiveTabProvider);
-    return _views[activeTabIndex];
+    return ref.watch(layoutActiveWidgetProvider);
   }
-
-  final List<Widget> _views = const [
-    AboutView(),
-    SkillsView(),
-    PortfolioView(),
-  ];
 }
