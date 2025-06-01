cb8327a49fc1bca519b6c010ff35594a78f7e783
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-06-01 14:31:53 +0300

    Built ProjectsSectionCard widget & Updated About screen UI

--- lib/features/about/presentation/view/widgets/about_view.dart
+++ lib/features/about/presentation/view/widgets/about_view.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../../data/models/about.dart' show About;
+import '../../../data/models/about.dart';
 import '../../../data/models/introduction_section.dart';
 import 'introduction_section_card.dart';
+import 'projects_section_card.dart';
 
 class AboutView extends StatelessWidget {
   const AboutView({super.key, required this.about});
@@ -25,6 +26,12 @@ class AboutView extends StatelessWidget {
                 seeMyWorkLink: about.seeMyWorkLink,
                 headerBigText: about.headerBigText,
               ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              margin: EdgeInsets.symmetric(vertical: 16.h),
+              child: ProjectsSectionCard(projects: about.projects),
             ),
           ),
         ],
---
+++ lib/features/about/presentation/view/widgets/projects_section_card.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
+
+import 'package:deplfolio/core/helpers/extensions.dart';
+
+import '../../../../../core/utils/app_strings.dart';
+import '../../../data/models/about.dart' show Project;
+import 'project_item.dart';
+
+class ProjectsSectionCard extends StatelessWidget {
+  const ProjectsSectionCard({super.key, required this.projects});
+
+  final List<Project> projects;
+
+  @override
+  Widget build(BuildContext context) {
+    return ShadCard(
+      radius: BorderRadius.circular(16.r),
+      columnMainAxisSize: MainAxisSize.min,
+      title: Text(AppStrings.projects, style: context.shadTextTheme.h4),
+      child: Wrap(
+        spacing: 16.w,
+        runSpacing: 16.h,
+        children:
+            projects.map((project) => ProjectItem(project: project)).toList(),
+      ),
+    );
+  }
+}
