53ffd103dfd44b1637432df3d386b7807fb90cf9
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-06-02 14:54:49 +0300

    Updated IntroductionSectionFormConsumer widget's inputs

--- lib/features/about/presentation/view/widgets/introduction_section_form_consumer.dart
+++ lib/features/about/presentation/view/widgets/introduction_section_form_consumer.dart
@@ -7,7 +7,6 @@ import '../../../../../core/helpers/input_validator.dart';
 import '../../../../../core/providers/autovalidate_mode_notifier.dart';
 import '../../../../../core/utils/app_strings.dart';
 import '../../../../../core/widgets/custom_data_input.dart';
-import '../../../../../core/widgets/primary_button.dart';
 import '../../../data/models/introduction_section.dart';
 import '../../providers/introduction_section_form_providers.dart';
 
@@ -22,22 +21,6 @@ class IntroductionSectionFormConsumer extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final formKey = ref.watch(introductionSectionFormKeyProvider);
-    final headerSmallTxtController = ref.watch(
-      aboutHeaderSmallTxtControllerProvider,
-    );
-    final descriptionController = ref.watch(aboutDescriptionControllerProvider);
-    final seeMyWorkLinkController = ref.watch(
-      aboutSeeMyWorkLinkControllerProvider,
-    );
-    final headerBigText1Controller = ref.watch(
-      aboutHeaderBigText1ControllerProvider,
-    );
-    final headerBigText2Controller = ref.watch(
-      aboutHeaderBigText2ControllerProvider,
-    );
-    final headerBigTextColoredStringController = ref.watch(
-      aboutHeaderBigTextColoredStringControllerProvider,
-    );
     final autovalidateMode = ref.watch(autovalidateModeProvider);
     return ShadForm(
       key: formKey,
@@ -47,64 +30,69 @@ class IntroductionSectionFormConsumer extends ConsumerWidget {
           Container(
             margin: EdgeInsets.only(top: 16.h),
             child: CustomDataInput(
-              controller: headerSmallTxtController,
               autovalidateMode: autovalidateMode,
               initialValue: introductionSection.headerSmallText,
               labelText: AppStrings.headerSmallText,
               textCapitalization: TextCapitalization.sentences,
               validator: (value) => InputValidator.validatingEmptyField(value),
+              onChanged: (value) {
+                ref.read(aboutHeaderSmallTxtProvider.notifier).state = value;
+              },
             ),
           ),
           CustomDataInput(
             initialValue: introductionSection.description,
-            controller: descriptionController,
             autovalidateMode: autovalidateMode,
             labelText: AppStrings.myDescription,
             textCapitalization: TextCapitalization.sentences,
             validator: (value) => InputValidator.validatingEmptyField(value),
+            onChanged: (value) {
+              ref.read(aboutDescriptionProvider.notifier).state = value;
+            },
           ),
           CustomDataInput(
             initialValue: introductionSection.seeMyWorkLink,
-            controller: seeMyWorkLinkController,
             autovalidateMode: autovalidateMode,
             labelText: AppStrings.seeMyWorkUrl,
             validator: (value) => InputValidator.validatingUrlField(value),
+            onChanged: (value) {
+              ref.read(aboutSeeMyWorkLinkProvider.notifier).state = value;
+            },
           ),
           Container(
             margin: EdgeInsets.only(top: 8.h),
             child: const Text(AppStrings.headerBigText),
           ),
           CustomDataInput(
-            controller: headerBigText1Controller,
             autovalidateMode: autovalidateMode,
             labelText: AppStrings.headerBigText1,
             initialValue: introductionSection.headerBigText.text1,
             textCapitalization: TextCapitalization.sentences,
             validator: (value) => InputValidator.validatingEmptyField(value),
+            onChanged: (value) {
+              ref.read(aboutHeaderBigText1Provider.notifier).state = value;
+            },
           ),
           CustomDataInput(
-            controller: headerBigText2Controller,
             autovalidateMode: autovalidateMode,
             labelText: AppStrings.headerBigText2,
             initialValue: introductionSection.headerBigText.text2,
             textCapitalization: TextCapitalization.sentences,
             validator: (value) => InputValidator.validatingEmptyField(value),
+            onChanged: (value) {
+              ref.read(aboutHeaderBigText2Provider.notifier).state = value;
+            },
           ),
           CustomDataInput(
-            controller: headerBigTextColoredStringController,
             autovalidateMode: autovalidateMode,
             labelText: AppStrings.headerBigTextColoredString,
             initialValue: introductionSection.headerBigText.coloredString,
             textCapitalization: TextCapitalization.sentences,
             validator: (value) => InputValidator.validatingEmptyField(value),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 16.h),
-            child: PrimaryButton(
-              expands: true,
-              text: AppStrings.saveChanges,
-              onPressed: () {},
-            ),
+            onChanged: (value) {
+              ref.read(aboutHeaderBigTextColoredStringProvider.notifier).state =
+                  value;
+            },
           ),
         ],
       ),
