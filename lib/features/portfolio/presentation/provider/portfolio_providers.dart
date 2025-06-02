09c327a918862a363a53c2ea170983f5b3519d18
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-06-02 11:34:33 +0300

    Finished fetchPortfolio UI logic 🚀

---
+++ lib/features/portfolio/presentation/provider/portfolio_providers.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../core/models/project.dart';
+import '../../../../core/supabase/supabase_request_result.dart';
+import '../../data/repository/portfolio_repo.dart';
+
+final fetchPortfolioProvider = FutureProvider.autoDispose<List<Project>>((
+  ref,
+) async {
+  final portfolioRepo = ref.read(portfolioRepoProvider);
+  final result = await portfolioRepo.fetchPortfolio(ref);
+  switch (result) {
+    case SupabaseRequestSuccess(:final data):
+      return data;
+    case SupabaseRequestFailure(:final errorModel):
+      throw errorModel.message;
+  }
+});
