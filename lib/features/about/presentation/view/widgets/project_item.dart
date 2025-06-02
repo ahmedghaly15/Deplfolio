87e030952e3077130b37cbb1d63e6dcc938aec8a
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2025-06-02 10:37:18 +0300

    Built UpdateProjectDialog widget & Updated ProjectItem widget

--- lib/features/about/presentation/view/widgets/project_item.dart
+++ lib/features/about/presentation/view/widgets/project_item.dart
@@ -3,11 +3,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:deplfolio/core/helpers/extensions.dart';
 
-import '../../../../../core/utils/app_strings.dart';
 import '../../../../../core/widgets/custom_cached_network_img.dart';
-import '../../../../../core/widgets/primary_button.dart';
 import '../../../data/models/about.dart';
-import 'project_details_dialog_content.dart';
+import 'update_project_dialog.dart';
 
 class ProjectItem extends StatelessWidget {
   const ProjectItem({super.key, required this.project});
@@ -18,13 +16,8 @@ class ProjectItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialButton(
       onPressed:
-          () => context.showDialog(
-            title: Text(project.title),
-            actions: [
-              PrimaryButton(onPressed: () {}, text: AppStrings.saveChanges),
-            ],
-            child: ProjectDetailsDialogContent(project: project),
-          ),
+          () =>
+              context.showDialog((_) => UpdateProjectDialog(project: project)),
       padding: EdgeInsets.zero,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.r)),
       child: Column(
