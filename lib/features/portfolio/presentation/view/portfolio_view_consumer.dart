7445cec6903cb6d1e241798f7ad3becbafe1d595
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-06-05 12:29:57 -0700

    Updated PortfolioViewConsumer widget

--- lib/features/portfolio/presentation/view/portfolio_view_consumer.dart
+++ lib/features/portfolio/presentation/view/portfolio_view_consumer.dart
@@ -1,14 +1,11 @@
-import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/theming/color_manager.dart';
-import '../../../../core/utils/app_utils.dart';
 import '../../../../core/widgets/adaptive_circular_progress_indicator.dart';
 import '../../../../core/widgets/adaptive_refresh_indicator.dart';
 import '../../../../core/widgets/custom_error_widget.dart';
-import 'widgets/project_item.dart';
+import 'widgets/portfolio_view.dart';
 import '../providers/fetch_portfolio_provider.dart';
 
 class PortfolioViewConsumer extends ConsumerWidget {
@@ -21,15 +18,7 @@ class PortfolioViewConsumer extends ConsumerWidget {
       data:
           (projects) => AdaptiveRefreshIndicator(
             onRefresh: () => ref.refresh(fetchPortfolioProvider.future),
-            child: ListView.separated(
-              physics: const BouncingScrollPhysics(),
-              padding: AppUtils.screenPadding,
-              itemBuilder:
-                  (_, index) =>
-                      ElasticIn(child: ProjectItem(project: projects[index])),
-              separatorBuilder: (_, __) => SizedBox(height: 24.h),
-              itemCount: projects.length,
-            ),
+            child: PortfolioView(projects: projects),
           ),
       error:
           (error, _) => CustomErrorWidget(
