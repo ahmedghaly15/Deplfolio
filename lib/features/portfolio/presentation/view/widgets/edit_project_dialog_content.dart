a76bdc1cf49445b69c56f72fa5ab5d0645064d48
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-06-06 04:40:25 -0700

    Built ProjectImgPickerConsumer & Updated EditProjectDialogContent widget

--- lib/features/portfolio/presentation/view/widgets/edit_project_dialog_content.dart
+++ lib/features/portfolio/presentation/view/widgets/edit_project_dialog_content.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:shadcn_ui/shadcn_ui.dart' show LucideIcons;
 
 import '../../../../../core/models/project.dart';
-import '../../../../../core/widgets/custom_cached_network_img.dart';
+
 import 'edit_project_dialog_form_consumer.dart';
+import 'project_img_picker_consumer.dart';
 
 class EditProjectDialogContent extends StatelessWidget {
   const EditProjectDialogContent({super.key, required this.project});
@@ -18,21 +18,7 @@ class EditProjectDialogContent extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       spacing: 16.h,
       children: [
-        Stack(
-          alignment: AlignmentDirectional.bottomEnd,
-          children: [
-            AspectRatio(
-              aspectRatio: 4,
-              child: CustomCachedNetworkImg(
-                imageUrl: project.imgPath,
-                imageBuilder:
-                    (_, img) =>
-                        CircleAvatar(backgroundImage: img, radius: 24.r),
-              ),
-            ),
-            IconButton(onPressed: () {}, icon: const Icon(LucideIcons.pen400)),
-          ],
-        ),
+        ProjectImgPickerConsumer(projectImgPath: project.imgPath),
         EditProjectDialogFormConsumer(project: project),
       ],
     );
---
+++ lib/features/portfolio/presentation/view/widgets/project_img_picker_consumer.dart
@@ -0,0 +1,48 @@
+import 'dart:io' show File;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shadcn_ui/shadcn_ui.dart' show LucideIcons;
+
+import '../../../../../core/widgets/custom_cached_network_img.dart';
+import '../../providers/image_picker_providers.dart'
+    show imagePickerNotifierProvider;
+
+class ProjectImgPickerConsumer extends ConsumerWidget {
+  const ProjectImgPickerConsumer({super.key, required this.projectImgPath});
+
+  final String projectImgPath;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pickedImg = ref.watch(imagePickerNotifierProvider);
+    return Stack(
+      alignment: AlignmentDirectional.bottomEnd,
+      children: [
+        AspectRatio(
+          aspectRatio: 4,
+          child:
+              pickedImg == null
+                  ? CustomCachedNetworkImg(
+                    imageUrl: projectImgPath,
+                    imageBuilder:
+                        (_, img) =>
+                            CircleAvatar(backgroundImage: img, radius: 24.r),
+                  )
+                  : CircleAvatar(
+                    backgroundImage:
+                        FileImage(File(pickedImg.path)) as ImageProvider,
+                    radius: 24.r,
+                  ),
+        ),
+        IconButton.filled(
+          onPressed: () {
+            ref.read(imagePickerNotifierProvider.notifier).pickImage();
+          },
+          icon: const Icon(LucideIcons.pen400, color: Colors.white),
+        ),
+      ],
+    );
+  }
+}
