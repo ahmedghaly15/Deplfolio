7fb842614b7ec9bc27ef644c5c5633f7a8a88308
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-06-06 09:50:30 -0700

    Built ReadPickedCodeFile provider

---
+++ lib/features/code_editor/presentation/providers/read_picked_code_file_provider.dart
@@ -0,0 +1,48 @@
+import 'dart:io' show File;
+
+import 'package:permission_handler/permission_handler.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import 'pick_code_file_provider.dart';
+
+part 'read_picked_code_file_provider.g.dart';
+
+@riverpod
+class ReadPickedCodeFile extends _$ReadPickedCodeFile {
+  @override
+  AsyncValue<String?> build() => const AsyncValue.data(null);
+
+  void execute() async {
+    state = const AsyncLoading();
+    try {
+      final pickedFileContent = await _requestPermissionAndReadCodeFile();
+      state = AsyncData(pickedFileContent);
+    } catch (e) {
+      state = AsyncError(e.toString(), StackTrace.current);
+    }
+  }
+
+  Future<String?> _requestPermissionAndReadCodeFile() async {
+    final status = await Permission.storage.request();
+    if (status.isGranted) {
+      return await _readPickedCodeFileContent();
+    } else if (status.isDenied) {
+      await Permission.storage.request();
+    } else if (status.isPermanentlyDenied) {
+      await openAppSettings();
+    }
+
+    return null;
+  }
+
+  Future<String?> _readPickedCodeFileContent() async {
+    final pickedFilePath =
+        await ref.read(pickCodeFilePathProvider.notifier).pickCodeFilePath();
+    if (pickedFilePath != null) {
+      final file = File(pickedFilePath);
+      return await file.readAsString();
+    } else {
+      return null;
+    }
+  }
+}
---
+++ lib/features/code_editor/presentation/providers/read_picked_code_file_provider.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'read_picked_code_file_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$readPickedCodeFileHash() =>
+    r'5d1f914aa3e2ed6c6b55cca871180830633a4dcb';
+
+/// See also [ReadPickedCodeFile].
+@ProviderFor(ReadPickedCodeFile)
+final readPickedCodeFileProvider = AutoDisposeNotifierProvider<
+  ReadPickedCodeFile,
+  AsyncValue<String?>
+>.internal(
+  ReadPickedCodeFile.new,
+  name: r'readPickedCodeFileProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$readPickedCodeFileHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ReadPickedCodeFile = AutoDisposeNotifier<AsyncValue<String?>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
