010e05c5c92a6484904b5d4f071f7be5a6fa5a6e
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-06-06 08:36:28 -0700

    Built pickCodeFileProvider

--- lib/features/code_editor/presentation/providers/code_editor_providers.dart
+++ lib/features/code_editor/presentation/providers/code_editor_providers.dart
@@ -1,8 +1,35 @@
+import 'dart:io' show File;
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter_code_editor/flutter_code_editor.dart'
     show CodeController;
-import 'package:flutter_riverpod/flutter_riverpod.dart' show Provider;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:highlight/languages/dart.dart';
 
 final codeEditorControllerProvider = Provider.autoDispose<CodeController>(
   (ref) => CodeController(language: dart),
 );
+
+final pickCodeFileProvider = FutureProvider.autoDispose<String?>((ref) async {
+  try {
+    return await _pickCodeFile();
+  } catch (e) {
+    throw e.toString();
+  }
+});
+
+Future<String?> _pickCodeFile() async {
+  final result = await FilePicker.platform.pickFiles(
+    type: FileType.custom,
+    allowedExtensions: ['dart', 'txt'],
+  );
+
+  final filePath = result?.files.single.path;
+
+  if (result != null && filePath != null) {
+    final file = File(filePath);
+    return await file.readAsString();
+  } else {
+    return null;
+  }
+}
