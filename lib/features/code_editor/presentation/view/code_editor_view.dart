23d876e0f4d0104495dd5da8ada40f2f8860898d
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-06-06 08:27:44 -0700

    Updated CodeEditorView

--- lib/features/code_editor/presentation/view/code_editor_view.dart
+++ lib/features/code_editor/presentation/view/code_editor_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_code_editor/flutter_code_editor.dart';
 import 'package:flutter_highlight/themes/atom-one-dark.dart'
     show atomOneDarkTheme;
+import 'package:shadcn_ui/shadcn_ui.dart' show LucideIcons;
 
 import 'code_field_consumer.dart';
 
@@ -12,7 +13,26 @@ class CodeEditorView extends StatelessWidget {
   Widget build(BuildContext context) {
     return CodeTheme(
       data: CodeThemeData(styles: atomOneDarkTheme),
-      child: const SingleChildScrollView(child: CodeFieldConsumer()),
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(LucideIcons.fileText400),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(LucideIcons.save400),
+              ),
+            ],
+          ),
+          const SliverFillRemaining(
+            hasScrollBody: false,
+            child: CodeFieldConsumer(),
+          ),
+        ],
+      ),
     );
   }
 }
