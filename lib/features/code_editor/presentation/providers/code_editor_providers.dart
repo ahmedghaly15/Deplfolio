5c1dc749d2c71c0853f0e6c414132a4e867381d7
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-06-06 08:24:59 -0700

    Built CodeFieldConsumer widget

---
+++ lib/features/code_editor/presentation/providers/code_editor_providers.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_code_editor/flutter_code_editor.dart'
+    show CodeController;
+import 'package:flutter_riverpod/flutter_riverpod.dart' show Provider;
+import 'package:highlight/languages/dart.dart';
+
+final codeEditorControllerProvider = Provider.autoDispose<CodeController>(
+  (ref) => CodeController(language: dart),
+);
---
+++ lib/features/code_editor/presentation/view/code_field_consumer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_code_editor/flutter_code_editor.dart' show CodeField;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+
+import '../providers/code_editor_providers.dart';
+
+class CodeFieldConsumer extends ConsumerWidget {
+  const CodeFieldConsumer({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final codeController = ref.watch(codeEditorControllerProvider);
+    return CodeField(controller: codeController);
+  }
+}
