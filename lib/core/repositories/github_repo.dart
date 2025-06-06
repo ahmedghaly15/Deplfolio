1d7431820538b9929b58e7fb4f6b02200a509494
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-06-06 14:40:18 -0700

    Built GitHubRepo

---
+++ lib/core/repositories/github_repo.dart
@@ -0,0 +1,48 @@
+import 'dart:convert' show base64Encode;
+import 'dart:io' show File;
+
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../api/api_request_result.dart';
+import '../api/github_api_service.dart';
+import '../models/update_remote_repo_file_params.dart';
+import '../models/update_remote_repo_file_request_body.dart';
+import '../utils/functions/api_execute_and_handle_errors.dart';
+
+final githubRepoProvider = Provider.autoDispose<GitHubRepo>((ref) {
+  final apiService = ref.read(githubApiServiceProvider);
+  return GitHubRepo(apiService);
+});
+
+class GitHubRepo {
+  final GitHubApiService _apiService;
+
+  GitHubRepo(this._apiService);
+
+  Future<String?> _checkForGithubFileExistence(String filePath) async {
+    final saferFilePathUrl = Uri.encodeComponent(filePath);
+    final response = await _apiService.checkForGithubFileExistence(
+      saferFilePathUrl,
+    );
+    return response.sha;
+  }
+
+  Future<ApiRequestResult<void>> updateRemoteRepoFile(
+    UpdateRemoteRepoFileParams params,
+  ) {
+    final saferFilePathUrl = Uri.encodeComponent(params.remoteFilePath);
+    final filePath = params.pickedFile.files.single.path!;
+    final fileBytes = File(filePath).readAsBytesSync();
+    final encodedContent = base64Encode(fileBytes);
+    return apiExecuteAndHandleErrors<void>(() async {
+      final sha = await _checkForGithubFileExistence(params.remoteFilePath);
+      await _apiService.updateRemoteFile(
+        saferFilePathUrl,
+        UpdateRemoteRepoFileRequestBody(
+          fileEncodedContent: encodedContent,
+          sha: sha,
+        ),
+      );
+    });
+  }
+}
