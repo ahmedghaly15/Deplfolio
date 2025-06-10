d8abdd9f762539dbe947013cf3a2e3e1926957f3
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-06-10 17:28:26 +0200

    Built UploadImgToGithub provider

---
+++ lib/features/portfolio/presentation/providers/upload_img_to_github_provider.dart
@@ -0,0 +1,32 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../../../core/api/api_request_result.dart';
+import '../../../../core/models/update_remote_repo_img_params.dart';
+import '../../../../core/repositories/github_repo.dart';
+import 'image_picker_providers.dart' show imagePickerNotifierProvider;
+
+part 'upload_img_to_github_provider.g.dart';
+
+@riverpod
+class UploadImgToGithub extends _$UploadImgToGithub {
+  @override
+  AsyncValue<void>? build() => null;
+
+  Future<void> execute(String projectTitle) async {
+    state = const AsyncValue.loading();
+    final result = await ref
+        .read(githubRepoProvider)
+        .updateRemoteRepoImg(
+          UpdateRemoteRepoImgParams(
+            projectTitle: projectTitle,
+            pickedFile: ref.read(imagePickerNotifierProvider)!,
+          ),
+        );
+    switch (result) {
+      case ApiRequestSuccess():
+        state = const AsyncValue.data(null);
+      case ApiRequestFailure(:final errorModel):
+        state = AsyncValue.error(errorModel.message, StackTrace.current);
+    }
+  }
+}
---
+++ lib/features/portfolio/presentation/providers/upload_img_to_github_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'upload_img_to_github_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$uploadImgToGithubHash() => r'2ebf739a32d5c6b01e0755f97f755b79336eb93d';
+
+/// See also [UploadImgToGithub].
+@ProviderFor(UploadImgToGithub)
+final uploadImgToGithubProvider =
+    AutoDisposeNotifierProvider<UploadImgToGithub, AsyncValue<void>?>.internal(
+      UploadImgToGithub.new,
+      name: r'uploadImgToGithubProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$uploadImgToGithubHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$UploadImgToGithub = AutoDisposeNotifier<AsyncValue<void>?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
