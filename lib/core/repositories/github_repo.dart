6d750779904f47aafbff2131aaa1a0df787b2aff
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-06-11 12:27:41 +0300

    Updated _checkForGithubFileExistence() in GitHubRepo

--- lib/core/repositories/github_repo.dart
+++ lib/core/repositories/github_repo.dart
@@ -5,6 +5,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../api/api_request_result.dart';
 import '../api/github_api_service.dart';
+import '../models/check_for_github_file_existence_response.dart';
 import '../models/update_remote_repo_file_params.dart';
 import '../models/update_remote_repo_file_request_body.dart';
 import '../models/update_remote_repo_img_params.dart';
@@ -22,16 +23,28 @@ class GitHubRepo {
   GitHubRepo(this._apiService);
 
   Future<String?> _checkForGithubFileExistence(String saferFilePathUrl) async {
-    final response = await _apiService.checkForGithubFileExistence(
-      saferFilePathUrl,
-    );
-    return response.sha;
+    final result =
+        await apiExecuteAndHandleErrors<CheckForGithubFileExistenceResponse>(
+          () => _apiService.checkForGithubFileExistence(saferFilePathUrl),
+        );
+
+    switch (result) {
+      case ApiRequestSuccess(data: final data):
+        return data.sha;
+      case ApiRequestFailure(errorModel: final errorModel):
+        if (errorModel.statusCode == 404 ||
+            errorModel.message.contains('Not Found')) {
+          return null; // File doesn't exist
+        }
+        throw errorModel.message;
+    }
   }
 
   Future<ApiRequestResult<void>> updateRemoteRepoFile(
     UpdateRemoteRepoFileParams params,
-  ) {
-    final saferFilePathUrl = Uri.encodeComponent(params.remoteFilePath);
+  ) async {
+    // Used Uri.encodeFull to encode only spaces or special characters
+    final saferFilePathUrl = Uri.encodeFull(params.remoteFilePath);
     final filePath = params.pickedFile?.files.single.path!;
     final fileBytes = File(filePath!).readAsBytesSync();
     final encodedContent = base64Encode(fileBytes);
@@ -51,8 +64,8 @@ class GitHubRepo {
   Future<ApiRequestResult<void>> updateRemoteRepoImg(
     UpdateRemoteRepoImgParams params,
   ) async {
-    final saferUrl = Uri.encodeComponent(
-      '${ConstStrings.assetsRemoteRepoFilePath}/${params.projectTitle}_icon.png',
+    final saferUrl = Uri.encodeFull(
+      '${ConstStrings.assetsRemoteRepoPath}images/${params.projectTitle.toLowerCase()}_icon.png',
     );
     final fileBytes = await params.pickedFile?.readAsBytes();
     final encodedContent = base64Encode(fileBytes!.toList());
