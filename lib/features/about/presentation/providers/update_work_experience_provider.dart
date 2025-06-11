65f4733b66fea6556ad0109b71f7632feb774961
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-06-11 13:24:59 +0300

    Finished UpdateWorkExperience provider

---
+++ lib/features/about/presentation/providers/update_work_experience_provider.dart
@@ -0,0 +1,98 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../../../core/enums/experience_status.dart';
+import '../../../../core/supabase/supabase_request_result.dart';
+import '../../data/models/about.dart' show WorkExperienceModel;
+import '../../data/repository/about_repo.dart';
+
+part 'update_work_experience_provider.g.dart';
+
+final workExperienceTitleProvider = StateProvider.autoDispose<String>(
+  (ref) => '',
+);
+
+final workExperienceStartDateProvider = StateProvider.autoDispose<String>(
+  (ref) => '',
+);
+
+final workExperienceEndDateProvider = StateProvider.autoDispose<String>(
+  (ref) => '',
+);
+
+final workExperienceCompanyProvider = StateProvider.autoDispose<String>(
+  (ref) => '',
+);
+
+final workExperienceStatusProvider = StateProvider.autoDispose
+    .family<ExperienceStatus, ExperienceStatus>(
+      (ref, initialStatus) => initialStatus,
+    );
+
+final workExperienceDescriptionProvider = StateProvider.autoDispose<String>(
+  (ref) => '',
+);
+
+@riverpod
+class UpdateWorkExperience extends _$UpdateWorkExperience {
+  @override
+  AsyncValue<void>? build() => null;
+
+  void execute(WorkExperienceModel workExperience) async {
+    final title = ref.read(workExperienceTitleProvider);
+    final startDate = ref.read(workExperienceStartDateProvider);
+    final endDate = ref.read(workExperienceEndDateProvider);
+    final company = ref.read(workExperienceCompanyProvider);
+    final status = ref.read(
+      workExperienceStatusProvider(workExperience.experienceStatus),
+    );
+    final description = ref.read(workExperienceDescriptionProvider);
+
+    final we = workExperience.copyWith(
+      id: workExperience.id,
+      title: title.isEmpty ? workExperience.title.trim() : title.trim(),
+      startDate:
+          startDate.isEmpty
+              ? workExperience.startDate.trim()
+              : startDate.trim(),
+      endDate: endDate.isEmpty ? workExperience.endDate.trim() : endDate.trim(),
+      company: company.isEmpty ? workExperience.company.trim() : company.trim(),
+      experienceStatus: status,
+      description:
+          description.isEmpty
+              ? workExperience.description.trim()
+              : description.trim(),
+    );
+
+    state = const AsyncValue.loading();
+    final result = await ref
+        .read(aboutRepoProvider)
+        .updateWorkExperience(ref, we);
+    switch (result) {
+      case SupabaseRequestSuccess():
+        state = const AsyncData(null);
+      case SupabaseRequestFailure(:final errorModel):
+        state = AsyncError(errorModel.message, StackTrace.current);
+    }
+  }
+}
+
+final isUpdateWeEnabledProvider = StateProvider.autoDispose
+    .family<bool, WorkExperienceModel>((ref, initialValues) {
+      final title = ref.watch(workExperienceTitleProvider);
+      final startDate = ref.watch(workExperienceStartDateProvider);
+      final endDate = ref.watch(workExperienceEndDateProvider);
+      final company = ref.watch(workExperienceCompanyProvider);
+      final status = ref.watch(
+        workExperienceStatusProvider(initialValues.experienceStatus),
+      );
+      final description = ref.watch(workExperienceDescriptionProvider);
+      final hasChanged =
+          (title.isNotEmpty && title != initialValues.title) ||
+          (startDate.isNotEmpty && startDate != initialValues.startDate) ||
+          (endDate.isNotEmpty && endDate != initialValues.endDate) ||
+          (company.isNotEmpty && company != initialValues.company) ||
+          (status != initialValues.experienceStatus) ||
+          (description.isNotEmpty && description != initialValues.description);
+      return hasChanged;
+    });
---
+++ lib/features/about/presentation/providers/update_work_experience_provider.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'update_work_experience_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$updateWorkExperienceHash() =>
+    r'd3dc3cd8d859d3e3e6a98378f3ce542bccf75369';
+
+/// See also [UpdateWorkExperience].
+@ProviderFor(UpdateWorkExperience)
+final updateWorkExperienceProvider = AutoDisposeNotifierProvider<
+  UpdateWorkExperience,
+  AsyncValue<void>?
+>.internal(
+  UpdateWorkExperience.new,
+  name: r'updateWorkExperienceProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$updateWorkExperienceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$UpdateWorkExperience = AutoDisposeNotifier<AsyncValue<void>?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
