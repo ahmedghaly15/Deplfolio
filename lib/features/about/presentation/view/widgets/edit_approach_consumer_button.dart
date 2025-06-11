f1c211d1209b35e3cf86dd30c065e8b4d7767189
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-06-11 16:25:51 +0300

    Built EditApproachConsumerButton widget

---
+++ lib/features/about/presentation/view/widgets/edit_approach_consumer_button.dart
@@ -0,0 +1,41 @@
+import 'package:deplfolio/core/helpers/extensions.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../../core/utils/app_strings.dart';
+import '../../../../../core/widgets/adaptive_circular_progress_indicator.dart';
+import '../../../../../core/widgets/primary_button.dart';
+import '../../../data/models/about.dart' show ApproachModel;
+import '../../providers/update_approach_provider.dart'
+    show updateApproachProvider;
+
+class EditApproachConsumerButton extends ConsumerWidget {
+  const EditApproachConsumerButton({super.key, required this.approach});
+
+  final ApproachModel approach;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncUpdateApproach = ref.watch(updateApproachProvider);
+    _updateApproachProviderListener(ref, context);
+    return PrimaryButton(
+      onPressed:
+          () => ref
+              .read(updateApproachProvider.notifier)
+              .validateAndUpdate(approach),
+      text: AppStrings.saveChanges,
+      child: asyncUpdateApproach?.whenOrNull(
+        loading: () => const AdaptiveCircularProgressIndicator(),
+      ),
+    );
+  }
+
+  void _updateApproachProviderListener(WidgetRef ref, BuildContext context) {
+    ref.listen(updateApproachProvider, (_, current) {
+      current?.whenOrNull(
+        data: (_) => context.showToast(AppStrings.aboutDataUpdateSuccess),
+        error: (error, _) => context.showToast(error.toString()),
+      );
+    });
+  }
+}
