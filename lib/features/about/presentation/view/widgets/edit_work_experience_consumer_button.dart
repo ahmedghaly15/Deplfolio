988591b0e9d358bab5d1b17ac1ced2f3bf35d26c
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-06-11 14:07:42 +0300

    Finished EditWorkExperienceConsumerButton widget

---
+++ lib/features/about/presentation/view/widgets/edit_work_experience_consumer_button.dart
@@ -0,0 +1,50 @@
+import 'package:deplfolio/core/helpers/extensions.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/app_strings.dart';
+import '../../../../../core/widgets/adaptive_circular_progress_indicator.dart';
+import '../../../../../core/widgets/primary_button.dart';
+import '../../../data/models/about.dart' show WorkExperienceModel;
+import '../../providers/update_work_experience_provider.dart'
+    show updateWorkExperienceProvider;
+
+class EditWorkExperienceConsumerButton extends ConsumerWidget {
+  const EditWorkExperienceConsumerButton({
+    super.key,
+    required this.workExperience,
+  });
+
+  final WorkExperienceModel workExperience;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final asyncUpdateWorkExperience = ref.watch(updateWorkExperienceProvider);
+    _updateWorkExperienceProviderListener(ref, context);
+    return PrimaryButton(
+      onPressed: () {
+        ref
+            .read(updateWorkExperienceProvider.notifier)
+            .validateAndUpdate(workExperience);
+      },
+      text: AppStrings.saveChanges,
+      child: asyncUpdateWorkExperience?.whenOrNull(
+        loading: () => const AdaptiveCircularProgressIndicator(),
+      ),
+    );
+  }
+
+  void _updateWorkExperienceProviderListener(
+    WidgetRef ref,
+    BuildContext context,
+  ) {
+    ref.listen(updateWorkExperienceProvider, (_, current) {
+      current?.whenOrNull(
+        data:
+            (_) =>
+                context.showToast(AppStrings.workExperienceUpdatedSuccessfully),
+        error: (error, _) => context.showToast(error.toString()),
+      );
+    });
+  }
+}
