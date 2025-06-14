a4196d18643736fc2f045c173a7050d77316bbb1
Ahmed Ghaly <108659381+ahmedghaly15@users.noreply.github.com>
2025-06-14 05:35:40 +0300

    Update build.gradle: Deploy 🚀

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
+    id "com.android.application"
+    // START: FlutterFire Configuration
+    id 'com.google.gms.google-services'
+    // END: FlutterFire Configuration
+    id "kotlin-android"
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("com.google.gms.google-services") 
-    id("dev.flutter.flutter-gradle-plugin")
+    id "dev.flutter.flutter-gradle-plugin"
 }
 
 android {
