f456fb8dabaab1325aa6cf9931982d01bb16c77e
Ahmed Ghaly <108659381+ahmedghaly15@users.noreply.github.com>
2025-06-14 11:25:08 +0300

    Updated settings.gradle.kts: Deploy 🚀

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -14,12 +14,15 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-}
 
-plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    plugins {
+        id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+        id("com.android.application") version "8.7.0" apply false
+        id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+
+        // ✅ Add this line only
+        id("com.google.gms.google-services") version "4.4.1"
+    }
 }
 
 include(":app")
