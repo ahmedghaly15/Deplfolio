1296102cff6f96d12783915d111200c36867175b
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-06-23 22:30:02 +0300

    Updated ShowProjectInAboutCheckboxConsumer widget  - Integrated toggleShowingProjectInAboutProvider UI logic with UI  - Integrated showOrHideProjectFromAboutProvider UI logic with UI

--- lib/features/portfolio/presentation/view/widgets/show_project_in_about_check_box_consumer.dart
+++ lib/features/portfolio/presentation/view/widgets/show_project_in_about_check_box_consumer.dart
@@ -1,8 +1,10 @@
+import 'package:deplfolio/core/helpers/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shadcn_ui/shadcn_ui.dart' show ShadCheckbox;
 
 import '../../../../../core/models/project.dart' show Project;
+import '../../../../../core/utils/app_strings.dart';
 import '../../providers/show_or_hide_project_from_about_provider.dart';
 
 class ShowProjectInAboutCheckboxConsumer extends ConsumerWidget {
@@ -12,21 +14,55 @@ class ShowProjectInAboutCheckboxConsumer extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isShown = ref.watch(
-      toggleShowOrHideProjectFromAboutProvider(project.shownInAbout),
-    );
+    final isShown = ref.watch(toggleShowingProjectInAboutProvider(project.id));
+    _showOrHideProjectFromAboutProviderListener(ref, context);
     return ShadCheckbox(
       value: isShown,
-      onChanged: (value) {
-        ref
-            .read(
-              toggleShowOrHideProjectFromAboutProvider(
-                project.shownInAbout,
-              ).notifier,
-            )
-            .state = !isShown;
-      },
+      onChanged: (value) => _showOrHideProjectFromAbout(ref),
       label: Text(project.title, overflow: TextOverflow.ellipsis, maxLines: 1),
     );
+  }
+
+  void _showOrHideProjectFromAboutProviderListener(
+    WidgetRef ref,
+    BuildContext context,
+  ) {
+    ref.listen(showOrHideProjectFromAboutProvider, (_, current) {
+      current?.whenOrNull(
+        data: (_) => _onShowOrHideSuccess(ref, context),
+        error: (error, _) => context.showToast(error.toString()),
+      );
+    });
+  }
+
+  void _onShowOrHideSuccess(WidgetRef ref, BuildContext context) {
+    final shownInAbout = ref.read(
+      toggleShowingProjectInAboutProvider(project.id),
+    );
+    switch (shownInAbout) {
+      case true:
+        context.showToast(AppStrings.projectIsShownInAbout);
+        break;
+      case false:
+        context.showToast(AppStrings.projectIsHiddenFromAbout);
+        break;
+    }
+  }
+
+  void _showOrHideProjectFromAbout(WidgetRef ref) {
+    final previousState = ref.read(
+      toggleShowingProjectInAboutProvider(project.id),
+    );
+    ref.read(toggleShowingProjectInAboutProvider(project.id).notifier).toggle();
+    ref
+        .read(showOrHideProjectFromAboutProvider.notifier)
+        .execute(
+          project.id,
+          onError: () {
+            ref
+                .read(toggleShowingProjectInAboutProvider(project.id).notifier)
+                .setValue(previousState);
+          },
+        );
   }
 }
