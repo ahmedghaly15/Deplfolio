a0284640761b7796f3840640ed06ab36232fba4d
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-06-23 21:49:25 +0300

    Updated portfolioProjects provider

--- lib/features/portfolio/presentation/providers/fetch_portfolio_provider.dart
+++ lib/features/portfolio/presentation/providers/fetch_portfolio_provider.dart
@@ -4,9 +4,16 @@ import '../../../../core/models/project.dart';
 import '../../../../core/supabase/supabase_request_result.dart';
 import '../../data/repository/portfolio_repo.dart';
 
-final portfolioProjectsProvider = StateProvider.autoDispose<List<Project>>(
-  (ref) => [],
-);
+final portfolioProjectsProvider = StateProvider.autoDispose<List<Project>>((
+  ref,
+) {
+  final link = ref.keepAlive();
+  // Dispose when the last listener is gone
+  ref.onCancel(() {
+    link.close(); // Allow disposal
+  });
+  return [];
+});
 
 final fetchPortfolioProvider = FutureProvider.autoDispose<List<Project>>((
   ref,
