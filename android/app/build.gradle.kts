7ebe384cfcd1968943cdfefcb1c298f51416729c
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-06-26 15:39:20 +0300

    Added hard coded ndk version ✅

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 android {
     namespace = "com.example.deplfolio"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13599879"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
