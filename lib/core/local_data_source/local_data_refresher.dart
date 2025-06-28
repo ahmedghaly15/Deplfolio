d4cf50554f34f28db5e540fda573efc1f2966f27
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-06-28 14:13:33 +0300

    Handled refreshing skills

--- lib/core/local_data_source/local_data_refresher.dart
+++ lib/core/local_data_source/local_data_refresher.dart
@@ -5,6 +5,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart' show WidgetRef;
 import '../../features/about/data/data_source.dart/local_data_source/about_dao.dart'
     show aboutDaoProvider;
 import '../../features/about/presentation/providers/fetch_about_provider.dart';
+import '../../features/skills/data/data_source/local_data_source/skills_dao.dart';
+import '../../features/skills/presentation/provider/fetch_skills_providers.dart';
 
 class LocalDataRefresher {
   LocalDataRefresher._();
@@ -12,7 +14,14 @@ class LocalDataRefresher {
   static Future<void> refreshAbout(WidgetRef ref) async {
     final aboutDao = await ref.read(aboutDaoProvider.future);
     await aboutDao.deleteAll();
-    log('DELETED ALL ABOUT FROM LOCAL DB');
+    log('DELETED ALL ABOUT FROM LOCAL DB & REFRESHED ABOUT PROVIDER');
     ref.invalidate(fetchAboutProvider);
+  }
+
+  static Future<void> refreshSkills(WidgetRef ref) async {
+    final skillsDao = await ref.read(skillsDaoProvider.future);
+    await skillsDao.deleteAllSkills();
+    log('DELETED ALL SKILLS FROM LOCAL DB & REFRESHED SKILLS PROVIDER');
+    ref.invalidate(fetchSkillsProvider);
   }
 }
