4c7e80ac8b84fdab26af505ac664f6505e1c30a7
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-06-28 12:24:15 +0300

    Built AboutDao & AppDatabase

---
+++ lib/core/local_data_source/app_database.dart
@@ -0,0 +1,28 @@
+import 'package:floor/floor.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart' show FutureProvider;
+
+import '../../features/about/data/data_source.dart/local_data_source/about_dao.dart';
+import '../../features/about/data/data_source.dart/local_data_source/dto_converters.dart';
+import '../../features/about/data/models/about.dart' show About;
+
+part 'app_database.g.dart';
+
+final appDatabaseProvider = FutureProvider.autoDispose<AppDatabase>((
+  ref,
+) async {
+  final database =
+      await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+  ref.onDispose(database.close);
+  return database;
+});
+
+@Database(version: 1, entities: [About])
+@TypeConverters([
+  AboutHeaderTextModelConverter,
+  WorkExperienceModelListConverter,
+  ApproachModelListConverter,
+  ProjectListConverter,
+])
+abstract class AppDatabase extends FloorDatabase {
+  AboutDao get aboutDao;
+}
---
+++ lib/features/about/data/data_source.dart/local_data_source/about_dao.dart
@@ -0,0 +1,22 @@
+import 'package:floor/floor.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart' show FutureProvider;
+
+import '../../../../../core/local_data_source/app_database.dart';
+import '../../models/about.dart' show About;
+
+final aboutDaoProvider = FutureProvider.autoDispose<AboutDao>((ref) async {
+  final database = await ref.read(appDatabaseProvider.future);
+  return database.aboutDao;
+});
+
+@dao
+abstract class AboutDao {
+  @Query('SELECT * FROM About')
+  Future<About?> fetchAbout();
+
+  @insert
+  Future<void> insertAbout(About about);
+
+  @Query('DELETE FROM About')
+  Future<void> deleteAll();
+}
