f755ab5ea15b3b79a66b889093bcc442f833e492
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-06-28 12:41:01 +0300

    Used local data source in fetching About

--- lib/features/about/data/repository/about_repo.dart
+++ lib/features/about/data/repository/about_repo.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../core/supabase/supabase_request_result.dart';
 import '../../../../core/utils/functions/supabase_execute_and_handle_errors.dart';
 import '../data_source.dart/about_remote_data_source.dart';
+import '../data_source.dart/local_data_source/about_dao.dart';
 import '../models/about.dart';
 import '../models/introduction_section.dart';
 
@@ -16,11 +17,18 @@ class AboutRepo {
 
   AboutRepo(this._remoteDataSource);
 
-  Future<SupabaseRequestResult<About>> fetchAbout(Ref ref) {
-    return supabaseExecuteAndHandleErrors(
-      ref,
-      () async => await _remoteDataSource.fetchAbout(),
-    );
+  Future<SupabaseRequestResult<About>> fetchAbout(Ref ref) async {
+    final aboutDao = await ref.read(aboutDaoProvider.future);
+    final cachedAbout = await aboutDao.fetchAbout();
+    if (cachedAbout != null) {
+      return SupabaseRequestResult.success(cachedAbout);
+    } else {
+      return supabaseExecuteAndHandleErrors(ref, () async {
+        final about = await _remoteDataSource.fetchAbout();
+        await aboutDao.insertAbout(about);
+        return about;
+      });
+    }
   }
 
   Future<SupabaseRequestResult<void>> updateIntroductionSection(
