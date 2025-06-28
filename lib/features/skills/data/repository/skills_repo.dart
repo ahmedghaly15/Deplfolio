851a17447540c19c5fc4cf97d1729d2e7513a81b
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-06-28 14:12:17 +0300

    Handled caching skills into local db

--- lib/features/skills/data/repository/skills_repo.dart
+++ lib/features/skills/data/repository/skills_repo.dart
@@ -1,7 +1,11 @@
+import 'dart:developer' show log;
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../core/supabase/supabase_request_result.dart';
 import '../../../../core/utils/functions/supabase_execute_and_handle_errors.dart';
+import '../data_source/local_data_source/skills_dao.dart'
+    show skillsDaoProvider;
 import '../data_source/skills_remote_data_source.dart';
 import '../models/fetch_skills.dart';
 import '../models/skills_texts.dart';
@@ -16,11 +20,20 @@ class SkillsRepo {
 
   SkillsRepo(this._remoteDataSource);
 
-  Future<SupabaseRequestResult<FetchSkills>> fetchSkills(Ref ref) {
-    return supabaseExecuteAndHandleErrors(
-      ref,
-      () async => await _remoteDataSource.fetchSkills(),
-    );
+  Future<SupabaseRequestResult<FetchSkills>> fetchSkills(Ref ref) async {
+    final skillsDao = await ref.read(skillsDaoProvider.future);
+    final cachedSkills = await skillsDao.fetchSkills();
+    if (cachedSkills != null) {
+      log('FETCHED CACHED SKILLS FROM LOCAL DB');
+      return SupabaseRequestResult.success(cachedSkills);
+    } else {
+      return supabaseExecuteAndHandleErrors(ref, () async {
+        final skills = await _remoteDataSource.fetchSkills();
+        await skillsDao.insertSkills(skills);
+        log('FETCHED SKILLS FROM REMOTE SOURCE AND SAVED TO LOCAL DB');
+        return skills;
+      });
+    }
   }
 
   Future<SupabaseRequestResult<void>> updateSkillHeader(
