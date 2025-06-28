5c4b80fdc0ad110f53b9cb5615f52259f58e2a59
DESKTOP-8H0HA4B\aboghaly <ahmedghaly0767@gmail.com>
2025-06-28 16:08:44 +0300

    Handled caching in fetching portfolio  - Updated fetchPortfolio() method in PortfolioRepo

--- lib/features/portfolio/data/repository/portfolio_repo.dart
+++ lib/features/portfolio/data/repository/portfolio_repo.dart
@@ -1,9 +1,14 @@
+import 'dart:developer' show log;
+
+import 'package:deplfolio/core/helpers/extensions.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart' show XFile;
 
 import '../../../../core/models/project.dart';
 import '../../../../core/supabase/supabase_request_result.dart';
 import '../../../../core/utils/functions/supabase_execute_and_handle_errors.dart';
+import '../data_source/local_data_source/project_dao.dart'
+    show projectDaoProvider;
 import '../data_source/portfolio_remote_data_source.dart';
 import '../models/update_project_img_params.dart';
 
@@ -17,11 +22,20 @@ class PortfolioRepo {
 
   PortfolioRepo(this._remoteDataSource);
 
-  Future<SupabaseRequestResult<List<Project>>> fetchPortfolio(Ref ref) {
-    return supabaseExecuteAndHandleErrors<List<Project>>(
-      ref,
-      () async => await _remoteDataSource.fetchPortfolio(),
-    );
+  Future<SupabaseRequestResult<List<Project>>> fetchPortfolio(Ref ref) async {
+    final projectDao = await ref.read(projectDaoProvider.future);
+    final cachedProjectsEntity = await projectDao.fetchProjects();
+    final cachedProjects = cachedProjectsEntity?.toProjectModelList();
+    if (cachedProjects != null) {
+      log('FETCHED CACHED PROJECTS FROM LOCAL DB');
+      return SupabaseRequestResult.success(cachedProjects);
+    } else {
+      return supabaseExecuteAndHandleErrors<List<Project>>(ref, () async {
+        final remoteProjects = await _remoteDataSource.fetchPortfolio();
+        await projectDao.insertProjects(remoteProjects.toProjectEntityList());
+        return remoteProjects;
+      });
+    }
   }
 
   Future<SupabaseRequestResult<void>> updateProject(Ref ref, Project project) {
