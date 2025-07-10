11708189d6f092b5c9015e679f5c02b6d3d6a30e
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-07-10 18:21:35 +0300

    Built ProjectEntityListConverter

--- lib/features/portfolio/data/data_source/local_data_source/project_type_converter.dart
+++ lib/features/portfolio/data/data_source/local_data_source/project_type_converter.dart
@@ -1,14 +1,29 @@
 import 'dart:convert';
 
+import 'package:deplfolio/core/helpers/extensions.dart';
 import 'package:floor/floor.dart' show TypeConverter;
 
 import '../../../../../core/models/project.dart';
 
-class ProjectConverter extends TypeConverter<Project, String> {
+import 'project_entity.dart';
+
+class ProjectEntityListConverter
+    extends TypeConverter<List<ProjectEntity>, String> {
   @override
-  Project decode(String databaseValue) =>
-      Project.fromJson(jsonDecode(databaseValue));
+  List<ProjectEntity> decode(String databaseValue) {
+    final List<dynamic> decodedJson = jsonDecode(databaseValue);
+    return decodedJson
+        .map(
+          (json) =>
+              Project.fromJson(json as Map<String, dynamic>).toProjectEntity(),
+        )
+        .toList();
+  }
 
   @override
-  String encode(Project value) => jsonEncode(value.toJson());
+  String encode(List<ProjectEntity> value) {
+    final List<Map<String, dynamic>> jsonList =
+        value.map((e) => e.toProjectModel(e).toJson()).toList();
+    return jsonEncode(jsonList);
+  }
 }
